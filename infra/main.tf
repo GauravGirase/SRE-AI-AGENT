be2a0117afc1677e6a6738b7f9d635c2b491a362
Gaurav <gaurav.girase111@gmail.com>
2026-03-18 18:53:56 +0530

    added agentcore execution role and agentcore memory

--- infra/main.tf
+++ infra/main.tf
@@ -599,3 +599,128 @@ resource "aws_bedrockagentcore_gateway_target" "agentcore_gateway_prometheus_tar
       }
     }
 }
+
+#######################################################
+# Agentcore execution 
+#######################################################
+resource "aws_iam_role" "agentcore_runtime_execution_role" {
+    name = "${var.app_name}-AgentCoreRuntime"
+    description = "Execution role for bedrock agentcore runtime"
+    assume_role_policy = data.aws_iam_policy_document.bedrock_agentcore_assume_role.json
+}
+
+resource "aws_iam_role_policy" "agentcore_runtime_execution_role_policy" {
+  role = aws_iam_role.agentcore_runtime_execution_role.id
+  name = "${var.app_name}-AgentCoreRuntimeExecutionPolicy"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "ECRImageAccess"
+        Effect = "Allow"
+        Action = [
+          "ecr:BatchGetImage",
+          "ecr:GetDownloadUrlForLayer",
+        ]
+        Resource = [
+          "arn:aws:ecr:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:repository/*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogStreams",
+          "logs:CreateLogGroup",
+        ]
+        Resource = [
+          "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/bedrock-agentcore/runtimes/*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:DescribeLogGroups",
+        ]
+        Resource = [
+          "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:*",
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+        ]
+        Resource = [
+          "arn:aws:logs:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:log-group:/aws/bedrock-agentcore/runtimes/*:log-stream:*",
+        ]
+      },
+      {
+        Sid    = "ECRTokenAccess"
+        Effect = "Allow"
+        Action = [
+          "ecr:GetAuthorizationToken",
+        ]
+        Resource = "*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "xray:PutTraceSegments",
+          "xray:PutTelemetryRecords",
+          "xray:GetSamplingRules",
+          "xray:GetSamplingTargets",
+        ]
+        Resource = [
+          "*",
+        ]
+      },
+      {
+        Effect   = "Allow"
+        Resource = "*"
+        Action   = "cloudwatch:PutMetricData"
+        Condition = {
+          StringEquals = {
+            "cloudwatch:namespace" = "bedrock-agentcore"
+          }
+        }
+      },
+      {
+        Sid    = "GetAgentAccessToken"
+        Effect = "Allow"
+        Action = [
+          "bedrock-agentcore:GetWorkloadAccessToken",
+          "bedrock-agentcore:GetWorkloadAccessTokenForJWT",
+          "bedrock-agentcore:GetWorkloadAccessTokenForUserId",
+        ]
+        Resource = [
+          "arn:aws:bedrock-agentcore:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:workload-identity-directory/default",
+          "arn:aws:bedrock-agentcore:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:workload-identity-directory/default/workload-identity/agentName-*",
+        ]
+      },
+      {
+        Sid    = "BedrockModelInvocation"
+        Effect = "Allow"
+        Action = [
+          "bedrock:InvokeModel",
+          "bedrock:InvokeModelWithResponseStream",
+        ]
+        Resource = [
+          "arn:aws:bedrock:*::foundation-model/*",
+          "arn:aws:bedrock:${data.aws_region.current.region}:${data.aws_caller_identity.current.account_id}:*",
+        ]
+      },
+    ]
+  })
+}
+
+############################################
+# Agentcore memory
+############################################
+resource "aws_bedrockagentcore_memory" "agentcore_memory" {
+    name = "eks-sre-agent_Memory"
+    description = "Memory resource with 30 days event expiry"
+    event_expiry_duration = 30
+  
+}
+
